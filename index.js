0def0f2cbd184a0659042f5a4518981662d7da67
Guilherme Ventura <gventura@matterway.io>
2020-01-17 16:09:33 +0100

    Remove chalk

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-var fs = require("fs");
-var chalk = require("chalk");
+var colors = {
+  reset: "\x1b[0m",
+  cyan: "\x1b[36m"
+};
 
-var link = chalk.cyanBright;
+var color = color => text => `${color}${text}${colors.reset}`;
+var link = color(colors.cyan);
+
 var website = "https://danguilherme.github.io/en/";
 
 console.log(`\n\tAccess ${link(website)} to know more :)\n`);
